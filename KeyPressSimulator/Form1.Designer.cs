e0c1281d2635c65329e1eeb2dd06a4cab328b18f
Joao Silva <joao.silva@ifgoiano.edu.br>
2024-08-16 09:23:08 -0300

    first commit

---
+++ KeyPressSimulator/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace KeyPressSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wButton = new System.Windows.Forms.Label();
+            this.aButton = new System.Windows.Forms.Label();
+            this.sButton = new System.Windows.Forms.Label();
+            this.dButton = new System.Windows.Forms.Label();
+            this.spaceButton = new System.Windows.Forms.Label();
+            this.qButton = new System.Windows.Forms.Label();
+            this.eButton = new System.Windows.Forms.Label();
+            this.rButton = new System.Windows.Forms.Label();
+            this.tButton = new System.Windows.Forms.Label();
+            this.hpButton = new System.Windows.Forms.Label();
+            this.mpButton = new System.Windows.Forms.Label();
+            this.lpButton = new System.Windows.Forms.Label();
+            this.hkButton = new System.Windows.Forms.Label();
+            this.mkButton = new System.Windows.Forms.Label();
+            this.lkButton = new System.Windows.Forms.Label();
+            this.fButton = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wButton
+            // 
+            this.wButton.AutoSize = true;
+            this.wButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.wButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.wButton.Location = new System.Drawing.Point(83, 9);
+            this.wButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.wButton.Name = "wButton";
+            this.wButton.Size = new System.Drawing.Size(65, 53);
+            this.wButton.TabIndex = 5;
+            this.wButton.Text = "W";
+            // 
+            // aButton
+            // 
+            this.aButton.AutoSize = true;
+            this.aButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.aButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.aButton.Location = new System.Drawing.Point(36, 73);
+            this.aButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.aButton.Name = "aButton";
+            this.aButton.Size = new System.Drawing.Size(53, 53);
+            this.aButton.TabIndex = 8;
+            this.aButton.Text = "A";
+            // 
+            // sButton
+            // 
+            this.sButton.AutoSize = true;
+            this.sButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.sButton.Location = new System.Drawing.Point(95, 73);
+            this.sButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.sButton.Name = "sButton";
+            this.sButton.Size = new System.Drawing.Size(53, 53);
+            this.sButton.TabIndex = 9;
+            this.sButton.Text = "S";
+            // 
+            // dButton
+            // 
+            this.dButton.AutoSize = true;
+            this.dButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.dButton.Location = new System.Drawing.Point(154, 73);
+            this.dButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.dButton.Name = "dButton";
+            this.dButton.Size = new System.Drawing.Size(55, 53);
+            this.dButton.TabIndex = 10;
+            this.dButton.Text = "D";
+            // 
+            // spaceButton
+            // 
+            this.spaceButton.AutoSize = true;
+            this.spaceButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.spaceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spaceButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.spaceButton.Location = new System.Drawing.Point(215, 142);
+            this.spaceButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.spaceButton.Name = "spaceButton";
+            this.spaceButton.Size = new System.Drawing.Size(171, 53);
+            this.spaceButton.TabIndex = 16;
+            this.spaceButton.Text = "SPACE";
+            // 
+            // qButton
+            // 
+            this.qButton.AutoSize = true;
+            this.qButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.qButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.qButton.Location = new System.Drawing.Point(20, 9);
+            this.qButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.qButton.Name = "qButton";
+            this.qButton.Size = new System.Drawing.Size(57, 53);
+            this.qButton.TabIndex = 1;
+            this.qButton.Text = "Q";
+            // 
+            // eButton
+            // 
+            this.eButton.AutoSize = true;
+            this.eButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.eButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.eButton.Location = new System.Drawing.Point(156, 9);
+            this.eButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.eButton.Name = "eButton";
+            this.eButton.Size = new System.Drawing.Size(53, 53);
+            this.eButton.TabIndex = 6;
+            this.eButton.Text = "E";
+            // 
+            // rButton
+            // 
+            this.rButton.AutoSize = true;
+            this.rButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.rButton.Location = new System.Drawing.Point(215, 9);
+            this.rButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.rButton.Name = "rButton";
+            this.rButton.Size = new System.Drawing.Size(55, 53);
+            this.rButton.TabIndex = 7;
+            this.rButton.Text = "R";
+            // 
+            // tButton
+            // 
+            this.tButton.AutoSize = true;
+            this.tButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.tButton.Location = new System.Drawing.Point(276, 9);
+            this.tButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.tButton.Name = "tButton";
+            this.tButton.Size = new System.Drawing.Size(50, 53);
+            this.tButton.TabIndex = 17;
+            this.tButton.Text = "T";
+            // 
+            // hpButton
+            // 
+            this.hpButton.AutoSize = true;
+            this.hpButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hpButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.hpButton.Location = new System.Drawing.Point(670, 9);
+            this.hpButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.hpButton.Name = "hpButton";
+            this.hpButton.Size = new System.Drawing.Size(84, 53);
+            this.hpButton.TabIndex = 20;
+            this.hpButton.Text = "HP";
+            // 
+            // mpButton
+            // 
+            this.mpButton.AutoSize = true;
+            this.mpButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mpButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.mpButton.Location = new System.Drawing.Point(575, 9);
+            this.mpButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.mpButton.Name = "mpButton";
+            this.mpButton.Size = new System.Drawing.Size(89, 53);
+            this.mpButton.TabIndex = 19;
+            this.mpButton.Text = "MP";
+            // 
+            // lpButton
+            // 
+            this.lpButton.AutoSize = true;
+            this.lpButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lpButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.lpButton.Location = new System.Drawing.Point(492, 9);
+            this.lpButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.lpButton.Name = "lpButton";
+            this.lpButton.Size = new System.Drawing.Size(77, 53);
+            this.lpButton.TabIndex = 18;
+            this.lpButton.Text = "LP";
+            // 
+            // hkButton
+            // 
+            this.hkButton.AutoSize = true;
+            this.hkButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hkButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.hkButton.Location = new System.Drawing.Point(670, 73);
+            this.hkButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.hkButton.Name = "hkButton";
+            this.hkButton.Size = new System.Drawing.Size(84, 53);
+            this.hkButton.TabIndex = 23;
+            this.hkButton.Text = "HK";
+            // 
+            // mkButton
+            // 
+            this.mkButton.AutoSize = true;
+            this.mkButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mkButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.mkButton.Location = new System.Drawing.Point(575, 73);
+            this.mkButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.mkButton.Name = "mkButton";
+            this.mkButton.Size = new System.Drawing.Size(89, 53);
+            this.mkButton.TabIndex = 22;
+            this.mkButton.Text = "MK";
+            // 
+            // lkButton
+            // 
+            this.lkButton.AutoSize = true;
+            this.lkButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lkButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.lkButton.Location = new System.Drawing.Point(492, 73);
+            this.lkButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.lkButton.Name = "lkButton";
+            this.lkButton.Size = new System.Drawing.Size(77, 53);
+            this.lkButton.TabIndex = 21;
+            this.lkButton.Text = "LK";
+            // 
+            // fButton
+            // 
+            this.fButton.AutoSize = true;
+            this.fButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fButton.ForeColor = System.Drawing.Color.AliceBlue;
+            this.fButton.Location = new System.Drawing.Point(213, 73);
+            this.fButton.MinimumSize = new System.Drawing.Size(32, 32);
+            this.fButton.Name = "fButton";
+            this.fButton.Size = new System.Drawing.Size(50, 53);
+            this.fButton.TabIndex = 11;
+            this.fButton.Text = "F";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(773, 202);
+            this.Controls.Add(this.hkButton);
+            this.Controls.Add(this.mkButton);
+            this.Controls.Add(this.lkButton);
+            this.Controls.Add(this.hpButton);
+            this.Controls.Add(this.mpButton);
+            this.Controls.Add(this.lpButton);
+            this.Controls.Add(this.tButton);
+            this.Controls.Add(this.spaceButton);
+            this.Controls.Add(this.fButton);
+            this.Controls.Add(this.dButton);
+            this.Controls.Add(this.sButton);
+            this.Controls.Add(this.aButton);
+            this.Controls.Add(this.rButton);
+            this.Controls.Add(this.eButton);
+            this.Controls.Add(this.wButton);
+            this.Controls.Add(this.qButton);
+            this.Name = "Form1";
+            this.Text = "Keyboard Overlay";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label wButton;
+        private Label aButton;
+        private Label sButton;
+        private Label dButton;
+        private Label spaceButton;
+        private Label qButton;
+        private Label eButton;
+        private Label rButton;
+        private Label tButton;
+        private Label hpButton;
+        private Label mpButton;
+        private Label lpButton;
+        private Label hkButton;
+        private Label mkButton;
+        private Label lkButton;
+        private Label fButton;
+    }
+}
+
---
+++ KeyPressSimulator/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KeyPressSimulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Evento para detectar quando uma tecla é pressionada
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aButton.ForeColor = Color.Black;
+                    aButton.BackColor = Color.White;
+                    break;
+                case Keys.S:
+                    sButton.ForeColor = Color.Black;
+                    sButton.BackColor = Color.White;
+                    break;
+                case Keys.D:
+                    dButton.ForeColor = Color.Black;
+                    dButton.BackColor = Color.White;
+                    break;
+                case Keys.Space:
+                    spaceButton.ForeColor = Color.Black;
+                    spaceButton.BackColor = Color.White;
+                    break;
+            }
+        }
+
+        // Evento para detectar quando uma tecla é solta
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    aButton.ForeColor = Color.White;
+                    aButton.BackColor = Color.Black;
+                    break;
+                case Keys.S:
+                    sButton.ForeColor = Color.White;
+                    sButton.BackColor = Color.Black;
+                    break;
+                case Keys.D:
+                    dButton.ForeColor = Color.White;
+                    dButton.BackColor = Color.Black;
+                    break;
+                case Keys.Space:
+                    spaceButton.ForeColor = Color.White;
+                    spaceButton.BackColor = Color.Black;
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}

35b5ef5f6bb64861ed94c22b199dedeef38b74cd
Joao Silva <joao.silva@ifgoiano.edu.br>
2024-08-16 12:05:42 -0300

    refatorando switch case

--- KeyPressSimulator/Form1.cs
+++ KeyPressSimulator/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -7,176 +8,59 @@ namespace KeyPressSimulator
 {
     public partial class Form1 : Form
     {
+        // Dicionario para guardar teclas.
+        private Dictionary<Keys, Control> keyButtonMap;
         public Form1()
         {
             InitializeComponent();
+            InitializeKeyButtonMap();
+        }
+
+        // Metodo que faz o mapeamento das teclas
+        private void InitializeKeyButtonMap() 
+        {
+            keyButtonMap = new Dictionary<Keys, Control>
+            {
+                {Keys.Q, qButton },
+                {Keys.W, wButton },
+                {Keys.E, eButton },
+                {Keys.R, rButton },
+                {Keys.T, tButton },
+                {Keys.A, aButton },
+                {Keys.S, sButton },
+                {Keys.D, dButton },
+                {Keys.F, fButton },
+                {Keys.Space, spaceButton },
+                {Keys.NumPad1, numpad1 },
+                {Keys.NumPad2, numpad2 },
+                {Keys.NumPad3, numpad3 },
+                {Keys.NumPad4, numpad4 },
+                {Keys.NumPad5, numpad5},
+                {Keys.NumPad6, numpad6 },
+                {Keys.NumPad7, numpad7 },
+                {Keys.NumPad8, numpad8 },
+                {Keys.NumPad9, numpad9 },
+            };
         }
 
         // Evento para detectar quando uma tecla é pressionada
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            switch (e.KeyCode)
+            if (keyButtonMap.ContainsKey(e.KeyCode))
             {
-                case Keys.A:
-                    aButton.ForeColor = Color.Black;
-                    aButton.BackColor = Color.White;
-                    break;
-                case Keys.S:
-                    sButton.ForeColor = Color.Black;
-                    sButton.BackColor = Color.White;
-                    break;
-                case Keys.D:
-                    dButton.ForeColor = Color.Black;
-                    dButton.BackColor = Color.White;
-                    break;
-                case Keys.F:
-                    fButton.ForeColor = Color.Black;
-                    fButton.BackColor = Color.White;
-                    break;
-                case Keys.Space:
-                    spaceButton.ForeColor = Color.Black;
-                    spaceButton.BackColor = Color.White;
-                    break;
-                case Keys.Q:
-                    qButton.ForeColor = Color.Black;
-                    qButton.BackColor = Color.White;
-                    break;
-                case Keys.W:
-                    wButton.ForeColor = Color.Black;
-                    wButton.BackColor = Color.White;
-                    break;
-                case Keys.E:
-                    eButton.ForeColor = Color.Black;
-                    eButton.BackColor = Color.White;
-                    break;
-                case Keys.R:
-                    rButton.ForeColor = Color.Black;
-                    rButton.BackColor = Color.White;
-                    break;
-                case Keys.T:
-                    tButton.ForeColor = Color.Black;
-                    tButton.BackColor = Color.White;
-                    break;
-                case Keys.NumPad1:
-                    numpad1.ForeColor = Color.Black;
-                    numpad1.BackColor = Color.White;
-                    break;
-                case Keys.NumPad2:
-                    numpad2.ForeColor = Color.Black;
-                    numpad2.BackColor = Color.White;
-                    break;
-                case Keys.NumPad3:
-                    numpad3.ForeColor = Color.Black;
-                    numpad3.BackColor = Color.White;
-                    break;
-                case Keys.NumPad4:
-                    numpad4.ForeColor = Color.Black;
-                    numpad4.BackColor = Color.White;
-                    break;
-                case Keys.NumPad5:
-                    numpad5.ForeColor = Color.Black;
-                    numpad5.BackColor = Color.White;
-                    break;
-                case Keys.NumPad6:
-                    numpad6.ForeColor = Color.Black;
-                    numpad6.BackColor = Color.White;
-                    break;
-                case Keys.NumPad7:
-                    numpad7.ForeColor = Color.Black;
-                    numpad7.BackColor = Color.White;
-                    break;
-                case Keys.NumPad8:
-                    numpad8.ForeColor = Color.Black;
-                    numpad8.BackColor = Color.White;
-                    break;
-                case Keys.NumPad9:
-                    numpad9.ForeColor = Color.Black;
-                    numpad9.BackColor = Color.White;
-                    break;
+                keyButtonMap[e.KeyCode].BackColor = Color.White;
+                keyButtonMap[e.KeyCode].ForeColor = Color.Black;
+                
             }
         }
 
         // Evento para detectar quando uma tecla é solta
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            switch (e.KeyCode)
+            if (keyButtonMap.ContainsKey(e.KeyCode))
             {
-                case Keys.A:
-                    aButton.ForeColor = Color.White;
-                    aButton.BackColor = Color.Black;
-                    break;
-                case Keys.S:
-                    sButton.ForeColor = Color.White;
-                    sButton.BackColor = Color.Black;
-                    break;
-                case Keys.D:
-                    dButton.ForeColor = Color.White;
-                    dButton.BackColor = Color.Black;
-                    break;
-                case Keys.F:
-                    fButton.ForeColor = Color.White;
-                    fButton.BackColor = Color.Black;
-                    break;
-                case Keys.Space:
-                    spaceButton.ForeColor = Color.White;
-                    spaceButton.BackColor = Color.Black;
-                    break;
-                case Keys.Q:
-                    qButton.ForeColor = Color.White;
-                    qButton.BackColor = Color.Black;
-                    break;
-                case Keys.W:
-                    wButton.ForeColor = Color.White;
-                    wButton.BackColor = Color.Black;
-                    break;
-                case Keys.E:
-                    eButton.ForeColor = Color.White;
-                    eButton.BackColor = Color.Black;
-                    break;
-                case Keys.R:
-                    rButton.ForeColor = Color.White;
-                    rButton.BackColor = Color.Black;
-                    break;
-                case Keys.T:
-                    tButton.ForeColor = Color.White;
-                    tButton.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad1:
-                    numpad1.ForeColor = Color.White;
-                    numpad1.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad2:
-                    numpad2.ForeColor = Color.White;
-                    numpad2.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad3:
-                    numpad3.ForeColor = Color.White;
-                    numpad3.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad4:
-                    numpad4.ForeColor = Color.White;
-                    numpad4.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad5:
-                    numpad5.ForeColor = Color.White;
-                    numpad5.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad6:
-                    numpad6.ForeColor = Color.White;
-                    numpad6.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad7:
-                    numpad7.ForeColor = Color.White;
-                    numpad7.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad8:
-                    numpad8.ForeColor = Color.White;
-                    numpad8.BackColor = Color.Black;
-                    break;
-                case Keys.NumPad9:
-                    numpad9.ForeColor = Color.White;
-                    numpad9.BackColor = Color.Black;
-                    break;
+                keyButtonMap[e.KeyCode].BackColor = Color.Black;
+                keyButtonMap[e.KeyCode].ForeColor = Color.White;
             }
         }
 
